32318f278da29ae0c0835a38ad93cb4354392bf0
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-07-17 13:52:24 -0600

    create & add arrows that will be used to move imgs
    created the div and put the imgs inside added css
    to make it look nicer and more interactive

--- src/moduleCarousel.js
+++ src/moduleCarousel.js
@@ -1,6 +1,25 @@
 import './carouselModule.css';
+import triangle from '../img/triangle.svg'
 
-export default ( containerClass ) => {
-    const container = document.querySelector(`${containerClass}`);
-    container.classList.add('mainContainer');
+
+export default (containerClass) => {
+  const container = document.querySelector(`${containerClass}`);
+  container.classList.add('mainContainer');
+
+  const leftArrowDiv = document.createElement('div');
+  leftArrowDiv.classList.add('arrowDiv');
+  const leftArrowImg = document.createElement('img')
+  leftArrowImg.src = triangle;
+  leftArrowDiv.appendChild( leftArrowImg);
+
+  
+  const rightArrowDiv = document.createElement('div');
+  rightArrowDiv.classList.add('arrowDiv');
+  const rightArrowImg = document.createElement('img')
+  rightArrowImg.src = triangle;
+  rightArrowDiv.appendChild( rightArrowImg);
+
+
+  container.appendChild(leftArrowDiv);
+  container.appendChild(rightArrowDiv);
 };
